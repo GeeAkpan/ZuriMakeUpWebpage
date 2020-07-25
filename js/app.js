f539e51e8bc56d356db39b07ceda9732868550fd
Akpan Godswill <godswillyhurhighnezz@gmail.com>
2020-07-25 23:27:50 +0100

    new changes implemented

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
+$(window).on("load",function() {
+    $('.preloader').fadeOut('slow');
+ });
+ 
 //Initialize AOS
 AOS.init();
 
 //Initialize NanoGallery
 
 $('document').ready(function () {
-    $("#nanoGallery3").nanoGallery();
+    $('#nanoGallery3').nanoGallery();
 });
+
